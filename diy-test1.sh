48e2edd8d2cee6cdf9a928c6076832d3162195c2
cokyrain <75012356+cokyrain@users.noreply.github.com>
2022-02-21 22:36:58 +0800

    Update diy-test1.sh

--- diy-test1.sh
+++ diy-test1.sh
@@ -1,5 +1,7 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.10/KERNEL_TESTING_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
