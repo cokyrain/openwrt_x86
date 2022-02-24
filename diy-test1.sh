e65a48a37927d18db6fb821e7c59a2ec19dc2fb3
cokyrain <75012356+cokyrain@users.noreply.github.com>
2022-02-24 09:02:27 +0800

    Update diy-test1.sh

--- diy-test1.sh
+++ diy-test1.sh
@@ -1,6 +1,5 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
