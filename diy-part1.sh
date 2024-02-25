5832cd9582419bcb3225624ed3e8987b8681bc74
cokyrain <75012356+cokyrain@users.noreply.github.com>
2024-02-25 12:40:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,6 @@
 #sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
