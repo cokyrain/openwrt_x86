17d0ac23666d755d40a8607e2529d881f861b7d9
cokyrain <75012356+cokyrain@users.noreply.github.com>
2025-09-05 11:29:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,16 +13,23 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# 修改源
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
-echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+
+#添加源
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
+#echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+sed -i '1i\
+src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages\n\
+src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall\n\
+src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+
 
 # 注释掉 23.x luci 行
 sed -i 's/^\(src-git luci .*\(23\.[^;]*\)\)/#\1/' feeds.conf.default
