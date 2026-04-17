9ccb53ecb50a1dadae512e2b669b4d0b4dd49977
cokyrain <75012356+cokyrain@users.noreply.github.com>
2026-04-17 16:28:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,16 +19,16 @@ echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
 #echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-sed -i '1i\
-src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages\
-src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall\
-src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+#sed -i '1i\
+#src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages\
+#src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall\
+#src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 
 
 # 注释掉 23.x luci 行
-sed -i 's/^\(src-git luci .*\(23\.[^;]*\)\)/#\1/' feeds.conf.default
+#sed -i 's/^\(src-git luci .*\(23\.[^;]*\)\)/#\1/' feeds.conf.default
 # 取消注释 24.x luci 行
-sed -i 's/^#\(src-git luci .*\(24\.[^;]*\)\)/\1/' feeds.conf.default
+#sed -i 's/^#\(src-git luci .*\(24\.[^;]*\)\)/\1/' feeds.conf.default
 
 #增加github代理
 #sed -i 's| \(https://github.com\)| https://gh-proxy.com/\1|g' feeds.conf.default
